84f72b2ded8f820b67d4c3fc31fc547e7cf99311
MOUGANGCAMILA <camilamougang16@gmail.com>
2025-11-19 17:19:52 +0100

    Part_1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JOB-PORTAL-with-MERN-STACK_2
 A job management system built using MongoDB, Express, React and Node.js.
+
---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+var cors = require('cors');
+
+
+//database connection
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("DB Connected"))
+.catch((err) => console.log(err));
+
+
+
+//port
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
